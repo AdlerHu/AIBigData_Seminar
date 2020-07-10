b9127e6098c0a585fbd441d7ec5aba66e4dd04a7
AdlerHu <55276788+AdlerHu@users.noreply.github.com>
2020-07-10 15:38:28 +0800

    Update price_predict_Ubuntu.py

--- Banana/Multi_step_LSTM/price_predict_Ubuntu.py
+++ Banana/Multi_step_LSTM/price_predict_Ubuntu.py
@@ -57,7 +57,8 @@ def insert_database(db, cursor, predictions, market_no):
 
 
 def main():
-    markets = {1: ['price_prediction', '109', 'models/price/Taipei1.h5', 'price,d1_price,d1_origin_price,week_day']}
+    markets = {1: ['price_prediction', '109', 'models/price/Taipei1.h5', 'price,d1_price,d1_origin_price,week_day'],
+              3: ['price_prediction', '241', 'models/price/Sanchong.h5', 'price,d1_price,week_day']}
 
     for key in markets.keys():
         market_no = markets[key][1]
